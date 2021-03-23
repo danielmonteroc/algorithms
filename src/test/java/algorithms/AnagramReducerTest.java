6906cd136503785e5ffef15ca37f4d294678af62
Daniel Montero <ing.danielmontero@gmail.com>
2021-03-22 18:22:37 -0600

    Setting up junit 5

--- src/test/java/algorithms/AnagramReducerTest.java
+++ src/test/java/algorithms/AnagramReducerTest.java
@@ -3,20 +3,19 @@ package algorithms;
 import org.junit.jupiter.api.Test;
 
 import static algorithms.AnagramAddReduce.isAnagram;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class AnagramReducerTest {
 
     @Test
     void axlRose() {
-        assertTrue(isAnagram("axlrose", "oralsex"));
-        assertTrue(isAnagram("acaba", "aaacb"));
+        assertThat(isAnagram("axlrose", "oralsex")).isTrue();
+        assertThat(isAnagram("acaba", "aaacb")).isTrue();
     }
 
     @Test
     void notanagram() {
-        assertFalse(isAnagram("axelrose", "oralsex"));
-        assertFalse(isAnagram("acabad", "aaacb"));
+        assertThat(isAnagram("axelrose", "oralsex")).isFalse();
+        assertThat(isAnagram("acabad", "aaacb")).isFalse();
     }
 }
--- src/test/java/algorithms/AnagramTest.java
+++ src/test/java/algorithms/AnagramTest.java
@@ -2,20 +2,19 @@ package algorithms;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class AnagramTest {
 
     @Test
     void axlRose() {
-        assertTrue(Anagram.isAnagram("axlrose", "oralsex"));
-        assertTrue(Anagram.isAnagram("acaba", "aaacb"));
+        assertThat(Anagram.isAnagram("axlrose", "oralsex")).isTrue();
+        assertThat(Anagram.isAnagram("acaba", "aaacb")).isTrue();
     }
 
     @Test
     void notanagram() {
-        assertFalse(Anagram.isAnagram("axelrose", "oralsex"));
-        assertFalse(Anagram.isAnagram("acabad", "aaacb"));
+        assertThat(Anagram.isAnagram("axelrose", "oralsex")).isFalse();
+        assertThat(Anagram.isAnagram("acabad", "aaacb")).isFalse();
     }
 }
