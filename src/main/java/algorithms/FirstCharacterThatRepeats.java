6941c733f97bfe10d5c9865b6a889cd8169365f0
Daniel Montero <ing.danielmontero@gmail.com>
2021-03-22 18:38:29 -0600

    Setting up unit test for problem

--- src/main/java/algorithms/FirstCharacterThatRepeats.java
+++ src/main/java/algorithms/FirstCharacterThatRepeats.java
@@ -1,3 +1,24 @@
 package algorithms;
 
-public class FirstCharacterThatRepeats {}
+public class FirstCharacterThatRepeats {
+    /**
+     * Method must return first character repeated in a string if no characters repeated are found
+     * return zero
+     *
+     * <p>Examples input: "abcabc" Returns: 'a'
+     *
+     * <p>input: "abcbca" Returns: 'b'
+     *
+     * <p>input: "" returns: -1
+     *
+     * <p>input: "abcdefghijklmnopqrstuvwxyz" returns: -1
+     *
+     * <p>Acceptance criteria:
+     *
+     * <p>Run unit test (.gradlew test) successfully
+     */
+    public static char getFirstRepeatCharacter(String input) {
+
+        return 0;
+    }
+}
---
+++ src/test/java/algorithms/FirstCharacterThatRepeatsTest.java
@@ -0,0 +1,39 @@
+package algorithms;
+
+import org.junit.jupiter.api.Test;
+
+import static algorithms.FirstCharacterThatRepeats.getFirstRepeatCharacter;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FirstCharacterThatRepeatsTest {
+
+    @Test
+    void testAbcabc() {
+        assertThat(getFirstRepeatCharacter("abcabc")).isEqualTo('a');
+    }
+
+    @Test
+    void testAbcbca() {
+        assertThat(getFirstRepeatCharacter("abcbca")).isEqualTo('b');
+    }
+
+    @Test
+    void testEmpty() {
+        assertThat(getFirstRepeatCharacter("abcbca")).isEqualTo(0);
+    }
+
+    @Test
+    void testAa() {
+        assertThat(getFirstRepeatCharacter("aa")).isEqualTo('a');
+    }
+
+    @Test
+    void testAbba() {
+        assertThat(getFirstRepeatCharacter("abba")).isEqualTo('b');
+    }
+
+    @Test
+    void testNotRepeatedLongString() {
+        assertThat(getFirstRepeatCharacter("abcdefghijklmnopqrstuvwxyz")).isEqualTo(0);
+    }
+}
