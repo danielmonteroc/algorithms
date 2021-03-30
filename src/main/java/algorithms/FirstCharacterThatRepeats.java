c9cc44020ab6449c3608a85a2e34cbcf55711a0e
anirak99 <karinafnts@gmail.com>
2021-03-29 19:36:28 -0600

    Solving getFirstRepeatCharacter

--- src/main/java/algorithms/FirstCharacterThatRepeats.java
+++ src/main/java/algorithms/FirstCharacterThatRepeats.java
@@ -1,24 +1,39 @@
 package algorithms;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class FirstCharacterThatRepeats {
-    /**
-     * Method must return first character repeated in a string if no characters repeated are found
-     * return zero
-     *
-     * <p>Examples input: "abcabc" Returns: 'a'
-     *
-     * <p>input: "abcbca" Returns: 'b'
-     *
-     * <p>input: "" returns: 0
-     *
-     * <p>input: "abcdefghijklmnopqrstuvwxyz" returns: 0
-     *
-     * <p>Acceptance criteria:
-     *
-     * <p>Run unit test (.gradlew test) successfully
-     */
-    public static char getFirstRepeatCharacter(String input) {
-    //Test
-        return 0;
-    }
+	/**
+	 * Method must return first character repeated in a string if no characters
+	 * repeated are found return zero
+	 *
+	 * <p>
+	 * Examples input: "abcabc" Returns: 'a'
+	 *
+	 * <p>
+	 * input: "abcbca" Returns: 'b'
+	 *
+	 * <p>
+	 * input: "" returns: 0
+	 *
+	 * <p>
+	 * input: "abcdefghijklmnopqrstuvwxyz" returns: 0
+	 *
+	 * <p>
+	 * Acceptance criteria:
+	 *
+	 * <p>
+	 * Run unit test (.gradlew test) successfully
+	 */
+	public static char getFirstRepeatCharacter(String input) {
+		Set<Character> visited = new HashSet<>();
+		for (char c : input.toCharArray()) {
+			if (visited.contains(c)) {
+				return c;
+			}
+			visited.add(c);
+		}
+		return 0;
+	}
 }
