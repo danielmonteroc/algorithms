34eb0a190587a76662cde6ecc818d64d1772bb69
Daniel Montero <ing.danielmontero@gmail.com>
2021-03-31 18:42:45 -0600

    Adding more unit tests

--- src/test/java/algorithms/CharacterCountEncoderTest.java
+++ src/test/java/algorithms/CharacterCountEncoderTest.java
@@ -8,12 +8,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 class CharacterCountEncoderTest {
 
     @Test
+    void testEncodeLongerString() {
+        assertThat(encode("aaaaaaaaaaabbbbbbbbbbccccdabc")).isEqualTo("a11b10c4d1a1b1c1");
+    }
+
+    @Test
     void testEncodeLongString() {
         assertThat(encode("aaaabbbbccccdabc")).isEqualTo("a4b4c4d1a1b1c1");
     }
 
     @Test
     void testEncodeShortString() {
+        assertThat(encode("a")).isEqualTo("a1");
+    }
+
+    @Test
+    void testEncodeString() {
         assertThat(encode("abc")).isEqualTo("a1b1c1");
     }
 
